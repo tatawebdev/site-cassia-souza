781f684235c7cfde3dd72aa68acbd85d774207bb
Jeremias <jeremias.souza@acreditarbanco.com.br>
2025-10-26 10:17:31 -0300

    adiciona seção de rodapé com informações de contato, links úteis e formulário de inscrição para newsletter

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -78,15 +78,20 @@
                                 <div class="dropdown-menu drop-down-content">
                                     <ul class="list-unstyled drop-down-pages">
                                         <li class="nav-item"><a class="dropdown-item nav-link"
-                                                href="{{ route('servicos.consultoria.tributaria') }}">Consultoria Tributária</a></li>
+                                                href="{{ route('servicos.consultoria.tributaria') }}">Consultoria
+                                                Tributária</a></li>
                                         <li class="nav-item"><a class="dropdown-item nav-link"
-                                                href="{{ route('servicos.defesa.administrativa') }}">Defesa Administrativa</a></li>
+                                                href="{{ route('servicos.defesa.administrativa') }}">Defesa
+                                                Administrativa</a></li>
                                         <li class="nav-item"><a class="dropdown-item nav-link"
-                                                href="{{ route('servicos.planejamento.tributario') }}">Planejamento Tributário</a></li>
+                                                href="{{ route('servicos.planejamento.tributario') }}">Planejamento
+                                                Tributário</a></li>
                                         <li class="nav-item"><a class="dropdown-item nav-link"
-                                                href="{{ route('servicos.recuperacao.creditos') }}">Recuperação de Créditos</a></li>
+                                                href="{{ route('servicos.recuperacao.creditos') }}">Recuperação de
+                                                Créditos</a></li>
                                         <li class="nav-item"><a class="dropdown-item nav-link"
-                                                href="{{ route('servicos.conformidade.tributaria') }}">Conformidade Tributária</a></li>
+                                                href="{{ route('servicos.conformidade.tributaria') }}">Conformidade
+                                                Tributária</a></li>
                                     </ul>
                                 </div>
                             </li>
@@ -131,139 +136,13 @@
         </section>
     </div>
 
-{{  $slot }}
+    {{  $slot }}
 
 
 
     <!-- Footer -->
-    <section class="footer-con position-relative" id="footer">
-        <div class="container position-relative">
-            <div class="row">
-                <div class="col-12">
-                    <div class="upper_portion">
-                        <div class="heading">
-                            <h6 class="text-white">Subscription</h6>
-                            <h3 class="text-white mb-0">Subscribe to Our Newsletter</h3>
-                        </div>
-                        <form action="javascript:;">
-                            <div class="form-group position-relative mb-0">
-                                <input type="text" class="form_style" placeholder="Your email address" name="email">
-                                <button>Subscribe<i class="fa-solid fa-arrow-right"></i></button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="middle_portion">
-                <div class="row">
-                    <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
-                        <div class="logo-content">
-                            <a href="./index.html" class="footer-logo">
-                                <figure class="mb-0"><img width="150px" src="/assets/images/logo-acinzentado.png" alt=""></figure>
-                            </a>
-                            <p class="text-size-14">Nemo enim ipsam voluptatem quia voluptas sit asperna tur aut odit
-                                aut fugit, sed quia conseuntur magni dolor es eos rui ratione...</p>
-                            <ul class="list-unstyled mb-0 social-icons">
-                                <li><a href="https://www.tiktok.com/@cassia.souza.adv?_t=ZM-90f26k8kEKh&_r=1" class="text-decoration-none" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-tiktok social-networks"></i></a></li>
-                                <li><a href="https://twitter.com/i/flow/login" class="text-decoration-none"><i
-                                            class="fa-brands fa-x-twitter social-networks"></i></a></li>
-                                <li><a href="https://www.linkedin.com/login" class="text-decoration-none"><i
-                                            class="fa-brands fa-linkedin social-networks"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 col-lg-2 col-md-4 col-sm-4 col-6">
-                        <div class="links">
-                            <h4 class="heading">Useful Links</h4>
-                            <ul class="list-unstyled mb-0">
-                                <li><i class="fa-solid fa-arrow-right"></i><a href="./index.html"
-                                        class=" text-size-14 text text-decoration-none">Home</a></li>
-                                <li><i class="fa-solid fa-arrow-right"></i><a href="./about.html"
-                                        class=" text-size-14 text text-decoration-none">About</a></li>
-                                <li><i class="fa-solid fa-arrow-right"></i><a href="./practice-area.html"
-                                        class=" text-size-14 text text-decoration-none">Practice Areas</a></li>
-                                <li><i class="fa-solid fa-arrow-right"></i><a href="./case-studies.html"
-                                        class=" text-size-14 text text-decoration-none">Cases</a></li>
-                                <li><i class="fa-solid fa-arrow-right"></i><a href="./blog.html"
-                                        class=" text-size-14 text text-decoration-none">Blog</a></li>
-                                <li><i class="fa-solid fa-arrow-right"></i><a href="./contact.html"
-                                        class=" text-size-14 text text-decoration-none">Contact</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6">
-                        <div class="timing">
-                            <h4 class="heading">Office Hours</h4>
-                            <ul class="list-unstyled mb-0">
-                                <li>
-                                    <p>Monday – Saturday</p>
-                                </li>
-                                <li><span>12:00 – 14:45</span></li>
-                                <li>
-                                    <p>Sunday – Thursday</p>
-                                </li>
-                                <li><span>17.30 – 21.45</span></li>
-                                <li>
-                                    <p>Friday – Saturday</p>
-                                </li>
-                                <li><span class="mb-0">13.00 – 19.45</span></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-4 col-12">
-                        <div class="icon">
-                            <h4 class="heading">Contact Us</h4>
-                            <ul class="list-unstyled mb-0">
-                                <li class="text">
-                                    <i class="fa-solid fa-phone"></i>
-                                    <a href="tel:+568925896325" class="text-decoration-none">+5689 2589 6325</a>
-                                </li>
-                                <li class="text">
-                                    <i class="fa-solid fa-envelope"></i>
-                                    <a href="mailto:info@lawfinity.com"
-                                        class="text-decoration-none">Info@lawfinity.com</a>
-                                </li>
-                                <li class="text">
-                                    <i class="fa-solid fa-location-dot"></i>
-                                    <p class="address mb-0">21 King Street Melbourne, 3000, Australia</p>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div id="fixed-form-container">
-            <div class="image">
-                <figure class="footer-contactimage mb-0">
-                    <img src="/assets/images/footer-contactimage.png" alt="" class="img-fluid">
-                </figure>
-            </div>
-            <div class="body" style="display: none;">
-                <form action="javascript:;">
-                    <div class="form-group mb-0">
-                        <input type="text" class="form_style" placeholder="Name" name="name">
-                    </div>
-                    <div class="form-group mb-0">
-                        <input type="email" class="form_style" placeholder="Email" name="emailid">
-                    </div>
-                    <div class="form-group mb-0">
-                        <input type="tel" class="form_style" placeholder="Phone" name="phone">
-                    </div>
-                    <div class="form-group mb-0">
-                        <textarea class="form_style" placeholder="Message" rows="3" name="msg"></textarea>
-                    </div>
-                    <button type="submit" class="submit_now text-decoration-none">Submit</button>
-                </form>
-            </div>
-        </div>
-        <figure class="footer-sideimage mb-0">
-            <img src="/assets/images/footer-sideimage.png" alt="image" class="img-fluid">
-        </figure>
-        <div class="copyright">
-            <p class="mb-0">Copyright © lawfinity.com, All Rights Reserved 2024</p>
-        </div>
-    </section>
+    @include('footer')
+
     <!-- PRE LOADER -->
     <div class="loader-mask">
         <div class="loader">
---
+++ resources/views/footer.blade.php
@@ -0,0 +1,128 @@
+<section class="footer-con position-relative" id="footer">
+    <div class="container position-relative">
+        <div class="row">
+            <div class="col-12">
+                <div class="upper_portion" data-aos="fade-up">
+                    <div class="heading">
+                        <h6 class="text-white" style="font-family: Arial, sans-serif; font-size: 14px;">Newsletter
+                        </h6>
+                        <h3 class="text-white mb-0" style="font-family: 'Times New Roman', serif; font-size: 24px;">
+                            Receba novidades e dicas tributárias</h3>
+                    </div>
+                    <form action="javascript:;">
+                        <div class="form-group position-relative mb-0">
+                            <input type="text" class="form_style" placeholder="Seu e-mail" name="email">
+                            <button>Inscrever-se<i class="fa-solid fa-arrow-right"></i></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="middle_portion">
+            <div class="row">
+                <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
+                    <div class="logo-content">
+                        <a href="/" class="footer-logo">
+                            <figure class="mb-0" style="width: 250px;"><img style="width: 250px;"
+                                    src="./assets/images/logo-acinzentado.png" alt="logo"></figure>
+                        </a>
+                        <p class="text-size-14">
+                            Escritório especializado em advocacia tributária. Soluções personalizadas para empresas
+                            que buscam segurança, economia e regularidade fiscal.
+                        </p>
+                        <ul class="list-unstyled mb-0 social-icons">
+                            <li><a href="https://www.tiktok.com/@cassia.souza.adv?_t=ZM-90f26k8kEKh&_r=1"
+                                    class="text-decoration-none" target="_blank" rel="noopener noreferrer"><i
+                                        class="fa-brands fa-tiktok social-networks"></i></a></li>
+                            <li><a href="https://twitter.com/i/flow/login" class="text-decoration-none"><i
+                                        class="fa-brands fa-x-twitter social-networks"></i></a></li>
+                            <li><a href="https://www.linkedin.com/login" class="text-decoration-none"><i
+                                        class="fa-brands fa-linkedin social-networks"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-2 col-md-4 col-sm-4 col-6">
+                    <div class="links">
+                        <h4 class="heading">Links Úteis</h4>
+                        <ul class="list-unstyled mb-0">
+                            <li><i class="fa-solid fa-arrow-right"></i>
+                                <a href="/" class=" text-size-14 text text-decoration-none">Início</a>
+                            </li>
+                            <li><i class="fa-solid fa-arrow-right"></i>
+                                <a href="/sobre-nos" class=" text-size-14 text text-decoration-none">Quem Somos</a>
+                            </li>
+                            <li><i class="fa-solid fa-arrow-right"></i>
+                                <a href="http://blog.cassiasouzaadvocacia.com.br/" target="_blank" class=" text-size-14 text text-decoration-none">Blog</a>
+                            </li>
+                            <li><i class="fa-solid fa-arrow-right"></i>
+                                <a href="/contact" class=" text-size-14 text text-decoration-none">Contato</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6">
+                    <div class="timing">
+                        <h4 class="heading">Horário de Atendimento</h4>
+                        <ul class="list-unstyled mb-0">
+                            <li>
+                                <p>Segunda a Sexta</p>
+                            </li>
+                            <li><span>09:00 – 18:00</span></li>
+                            <li>
+                                <p>Sábado</p>
+                            </li>
+                            <li><span>09:00 – 13:00</span></li>
+                            <li>
+                                <p>Domingo</p>
+                            </li>
+                            <li><span class="mb-0">Fechado</span></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-4 col-12">
+                    <div class="icon">
+                        <h4 class="heading">Contato</h4>
+                        <ul class="list-unstyled mb-0">
+                            <li class="text">
+                                <i class="fa-solid fa-phone"></i>
+                                <a href="tel:+5511915201084" class="text-decoration-none">+55 11 91520 1084</a>
+                            </li>
+                            <li class="text">
+                                <i class="fa-solid fa-envelope"></i>
+                                <a href="mailto:contato@cassiasouzaadvocacia.com.br"
+                                    class="text-decoration-none">contato@cassiasouzaadvocacia.com.br</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="fixed-form-container">
+        <div class="image">
+            <figure class="footer-contactimage mb-0">
+                <img src="./assets/images/footer-contactimage.png" alt="imagem" class="img-fluid">
+            </figure>
+        </div>
+        <div class="body" style="display: none;">
+            <form action="javascript:;">
+                <div class="form-group mb-0">
+                    <input type="text" class="form_style" placeholder="Nome" name="name">
+                </div>
+                <div class="form-group mb-0">
+                    <input type="email" class="form_style" placeholder="E-mail" name="emailid">
+                </div>
+                <div class="form-group mb-0">
+                    <input type="tel" class="form_style" placeholder="Telefone" name="phone">
+                </div>
+                <div class="form-group mb-0">
+                    <textarea class="form_style" placeholder="Mensagem" rows="3" name="msg"></textarea>
+                </div>
+                <button type="submit" class="submit_now text-decoration-none">Enviar</button>
+            </form>
+        </div>
+    </div>
+    <div class="copyright">
+        <p class="mb-0">Copyright © cassiasouzaadvocacia.com.br, Todos os direitos reservados 2025</p>
+    </div>
+</section>
