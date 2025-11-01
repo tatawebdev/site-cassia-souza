5dabd902837d2afe40115cade1fe94b3c83531f0
Jeremias <jeremias.souza@acreditarbanco.com.br>
2025-11-01 08:14:44 -0300

    Refactor image paths to use absolute URLs for consistency across layout files and views; update logo image references in footer and home routes.

--- resources/views/livewire/about.blade.php
+++ resources/views/livewire/about.blade.php
@@ -1,5 +1,4 @@
 <div>
-
     @include('sobre-nos')
 
     <section class="succeed-con position-relative">
@@ -34,53 +33,61 @@
                         </ul>
                     </div>
                 </div>
-<div class="col-lg-6 col-md-12 col-sm-12 col-12">
-    <div class="succeed_wrapper" data-aos="zoom-in">
-        <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-6 col-12">
-                <div class="box">
-                    <div class="succeed-box">
-                        <figure class="icon">
-                            <i class="fa-solid fa-chart-line" style="color: #800080; font-size: 2em;"></i>
-                        </figure>
-                        <span class="text">Empresas mais lucrativas e seguras</span>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="col-lg-6 col-md-6 col-sm-6 col-12">
-                <div class="succeed-box">
-                    <figure class="icon">
-                        <i class="fa-solid fa-balance-scale" style="color: #800080; font-size: 2em;"></i>
-                    </figure>
-                    <span class="text">Estratégias criadas com inteligência tributária</span>
-                </div>
-            </div>
-        </div>
-        
-        <div class="succeed-downcontent">
-            <div class="row">
-                <div class="col-lg-6 col-md-6 col-sm-6 col-12">
-                    <div class="box">
-                        <div class="succeed-box">
-                            <figure class="icon">
-                                <i class="fa-solid fa-user-tie" style="color: #800080; font-size: 2em;"></i>
-                            </figure>
-                            <span class="text">Consultorias estratégicas entregues</span>
+                <div class="col-lg-6 col-md-12 col-sm-12 col-12">
+                    <div class="succeed_wrapper" data-aos="zoom-in">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+                                <div class="box">
+                                    <div class="succeed-box">
+                                        <figure class="icon">
+                                            <i class="fa-solid fa-chart-line"
+                                                style="color: #800080; font-size: 2em;"></i>
+                                        </figure>
+                                        <span class="text">Empresas mais lucrativas e seguras</span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+                                <div class="succeed-box">
+                                    <figure class="icon">
+                                        <i class="fa-solid fa-balance-scale"
+                                            style="color: #800080; font-size: 2em;"></i>
+                                    </figure>
+                                    <span class="text">Estratégias criadas com inteligência tributária</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="succeed-downcontent">
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+                                    <div class="box">
+                                        <div class="succeed-box">
+                                            <figure class="icon">
+                                                <i class="fa-solid fa-user-tie"
+                                                    style="color: #800080; font-size: 2em;"></i>
+                                            </figure>
+                                            <span class="text">Consultorias estratégicas entregues</span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+                                    <div class="box">
+                                        <div class="succeed-box">
+                                            <figure class="icon">
+                                                <i class="fa-solid fa-cogs" style="color: #800080; font-size: 2em;"></i>
+                                            </figure>
+                                            <sup class="plus">+</sup>
+                                            <span class="text">Soluções que simplificam o complexo</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                
-                <div class="col-lg-6 col-md-6 col-sm-6 col-12">
-                    <div class="succeed-box">
-                        <figure class="icon">
-                            <i class="fa-solid fa-cogs" style="color: #800080; font-size: 2em;"></i>
-                        </figure>
-                        <sup class="plus">+</sup>
-                        <span class="text">Soluções que simplificam o complexo</span>
-                    </div>
-                </div>
             </div>
         </div>
-    </div>
+    </section>
 </div>
